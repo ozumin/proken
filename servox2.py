ccca6a6985380a36ba8fa4ddf7f20dcccd3a4e98
Mizuo Nagayama <mizuo.n@asagi.waseda.jp>
2017-12-24 17:02:18 +0900

    to move two servos by PCA9685

--- servox2.py
+++ servox2.py
@@ -17,9 +17,9 @@ def set_servo_pulse(channel, pulse):
     pulse_length = 1000000 # 1,000,000 us per second
 #    pulse_length //= 60 # 60 Hz
     pulse_length //= 50 # 50 Hz
-    print('{0}us per period'.format(pulse_length))
+    print('%d us per period' % format(pulse_length))
     pulse_length //=4096 # 12 bits of resolution
-    print('{0}us per bit'.format(pulse_length))
+    print('%d us per bit' % format(pulse_length))
 #    pulse *= 1000
     pulse //= pulse_length
     pwm.set_pwm(channel, 0, pulse)
