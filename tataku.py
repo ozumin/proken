71c5d37e30e0559ddbf746f4aef470552d959a71
Mizuo Nagayama <mizuo.n@asagi.waseda.jp>
2018-01-08 16:52:21 +0900

    speaking feedback for tataku

--- tataku.py
+++ tataku.py
@@ -48,33 +48,33 @@ pattern = r'WHYPO WORD=\"(.*)\" CLASSID'
 #    time.sleep(1)
 #    pwm.set_pwm(5, 0, 450)
 #    time.sleep(1)
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((host,port))
-        while True:
-            data = sock.recv(bufsize)
-            buff.write(data.decode('utf-8'))
-            data = buff.getvalue().replace('> ', '>\n ')
-            if '\n' in data:
-                lines = data.splitlines()
-                for i in range(len(lines)-1):
-                    if lines[i] != '.':
-                    m = re.search(pattern, lines[i])
-                    if m:
-                        word = m.group(1)
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host,port))
+    while True:
+        data = sock.recv(bufsize)
+        buff.write(data.decode('utf-8'))
+        data = buff.getvalue().replace('> ', '>\n ')
+        if '\n' in data:
+            lines = data.splitlines()
+            for i in range(len(lines)-1):
+                if lines[i] != '.':
+                m = re.search(pattern, lines[i])
+                if m:
+                    word = m.group(1)
 
-                        if u('起きた') in word:
-                            print(word)
-                            break
-                buff.close()
-                buff = StringIO(u(''))
-                if lines[len(lines)-1] != '.':
-                    buff.write(lines[len(lines)-1])
+                    if u('起きた') in word:
+                        print(word)
+                        break
+            buff.close()
+            buff = StringIO(u(''))
+            if lines[len(lines)-1] != '.':
+                buff.write(lines[len(lines)-1])
 
-    except socket.error:
-        print('socket error')
-    except KeyboardInterrupt:
-        pass
+except socket.error:
+    print('socket error')
+except KeyboardInterrupt:
+    pass
 
 sock.close()
 
