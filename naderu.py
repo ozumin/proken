91a444ba8b0540b2ba8deccf5364a7279a8a2b17
Mizuo Nagayama <mizuo.n@asagi.waseda.jp>
2018-01-10 18:50:40 +0900

    music

--- naderu.py
+++ naderu.py
@@ -117,7 +117,7 @@ try:
                             janken(random.randint(1,3))
                         elif u('つまらない') in word:
                             print(word)
-                            cmd = 'mplayer -ao alsa:device=plughw=0.0 2.mp3'
+                            cmd = 'mplayer -ao alsa:device=plughw=2.0 2.mp3'
                             subprocess.call(cmd.strip().split(" ") )
             buff.close()
             buff = StringIO(u(''))
