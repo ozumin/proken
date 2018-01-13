9ae21abc713fe15755a85e41ecd74a42b666558c
Mizuo Nagayama <mizuo.n@asagi.waseda.jp>
2018-01-13 15:48:06 +0900

    jtalk naderu

--- naderu.py
+++ naderu.py
@@ -126,7 +126,7 @@ try:
                             time.sleep(1)
                         elif u('じゃんけん') in word:
                             print(word)
-                            if__name__=='__main__'
+                            if __name__=='__main__':
                                 main()
                             janken(random.randint(1,3))
                         elif u('つまらない') in word:
