45cf7e2415fd4f6efc9e16eef6da6cace1110f9c
Mizuo Nagayama <mizuo.n@asagi.waseda.jp>
2018-01-13 17:35:06 +0900

    jtalk naderu

--- naderu.py
+++ naderu.py
@@ -109,15 +109,18 @@ try:
                             time.sleep(1)
                         elif u('じゃんけん') in word:
                             print(word)
-                            cmd = 'aplay -D plughw:2,0 janken.wav'
-                            subprocess.call(cmd.strip().split(" ") )
+#                            cmd = 'aplay -D plughw:2,0 janken.wav'
+#                            subprocess.call(cmd.strip().split(" ") )
+                            pygame.mixer.init(frequency=44100)
+                            pygame.mixer.music.load("janken.wav")
+                            pygame.mixer.music.play(1)
+                            pygame.mixer.music.stop()
                             janken(random.randint(1,3))
                         elif u('つまらない') in word:
                             print(word)
                             pygame.mixer.init()
                             pygame.mixer.music.load("2.mp3")
                             pygame.mixer.music.play(1)
-                            pygame.mixer.init()
                         elif u('ありがとう') in word:
                             print(word)
                             pygame.mixer.music.stop()
